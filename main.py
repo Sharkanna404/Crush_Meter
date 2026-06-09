fa6c0f1818a13dd17c57334acdb15b6e5cf1259d
Hermione <hermione@heart-decoder.com>
2026-06-10 00:17:08 +0800

    chore: FastAPI 绑定 0.0.0.0:8080 以支持外网直接访问

--- main.py
+++ main.py
@@ -1116,4 +1116,4 @@ async def admin_get_feedback(id: int = None, key: str = ""):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
